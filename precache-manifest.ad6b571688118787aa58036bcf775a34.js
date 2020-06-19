2c7586b3ed0a234b986789d802079f37abc181ac
George Chetreanu <georgeleonard.chetreanu@cgm.com>
2020-06-19 11:10:09 +0300

    Updates

---
+++ precache-manifest.ad6b571688118787aa58036bcf775a34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad166a6618e8a39d61588070704f44f",
+    "url": "/stocks-app/index.html"
+  },
+  {
+    "revision": "37e0dc942bdfd051eef2",
+    "url": "/stocks-app/static/css/main.835e0254.chunk.css"
+  },
+  {
+    "revision": "8a49cef97cac795bd7c3",
+    "url": "/stocks-app/static/js/2.afae389d.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/stocks-app/static/js/2.afae389d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e0dc942bdfd051eef2",
+    "url": "/stocks-app/static/js/main.0deb304c.chunk.js"
+  },
+  {
+    "revision": "0467a7c48adc6191ab1b",
+    "url": "/stocks-app/static/js/runtime-main.9f6f07c0.js"
+  }
+]);
